652026a284b029d2331fc099d67fa26a275241ec
rodrigo-rodas-126 <68860681+rodrigo-rodas-126@users.noreply.github.com>
2021-05-04 18:48:39 -0600

    Add files via upload
    Corrección regex correos, servicios funcionando

--- app/Proyecto3/Proyecto3/views.py
+++ app/Proyecto3/Proyecto3/views.py
@@ -1,27 +1,107 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, FileResponse, Http404
 from django.template import Template, Context, loader
 from django.shortcuts import render
 from tkinter import filedialog
 import os
 import requests
+from django.views.decorators.csrf import csrf_exempt
+from django.core.files.storage import FileSystemStorage
+from Database import archivos_cargados
 
-"""
-def inicio(request):
+path_media = r'C:\Users\Humberto Morales\OneDrive\Escritorio\Rodrigo\app\Proyecto3'
 
-    main = loader.get_template('index.html')
-
-    documento = main.render({"nombre": 'hola'})
-
-    return HttpResponse(documento)
-"""
 def home(request):
     main = loader.get_template('index.html')
-
-    documento = main.render({"nombre": 'hola'})
-
+    documento = main.render()
     return HttpResponse(documento)
 
-def inicio(request):
-    response = requests.get('http://127.0.0.1:8800/inicio').json()
-    return render(request, 'index.html', {'respuesta':response})
+def home1(request):
+    archivos_cargados.clear()
+    response = requests.get('http://127.0.0.1:8800/reset').json()
+    print(archivos_cargados)
+    return HttpResponseRedirect('http://127.0.0.1:8000')
 
+@csrf_exempt 
+def filtros(request):
+    main = loader.get_template('home.html')
+    # {"respuesta": archivos_cargados[0], "respuesta_estadistica": archivos_cargados[1]}
+    documento = main.render({"respuesta": archivos_cargados[0], "respuesta_estadistica": archivos_cargados[1]})
+    return HttpResponse(documento)
+
+@csrf_exempt 
+def filtros1(request):
+    if request.method == 'POST':
+        parametro1 = request.POST['codigo']
+        #print(parametro1)
+        url = 'http://127.0.0.1:8800/grafico_errores'
+        codigo = {'codigo': parametro1}
+        x = requests.post(url, data = codigo)
+        print(x.text)
+        return HttpResponseRedirect('http://127.0.0.1:8000/filtros')
+
+@csrf_exempt
+def filtros2(request):
+    if request.method == 'POST':
+        parametro = request.POST['fecha']
+        #print(parametro)
+        url = 'http://127.0.0.1:8800/graficar_fechas'
+        fecha = {'fecha': parametro}
+        x1 = requests.post(url, data = fecha)
+        print(x1.text)
+        return HttpResponseRedirect('http://127.0.0.1:8000/filtros')
+
+def inicio(request):
+    response = requests.get('http://127.0.0.1:8800/inicio')
+    return render(request, 'index.html', {'respuesta':response.text})
+
+@csrf_exempt 
+def archivo(request):
+    archivo_entrada = ''
+    if request.method == 'POST':
+        myfile = request.FILES['ourfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        aux_file = open(str(path_media)+uploaded_file_url, 'r')
+        for el in aux_file.readlines():
+            archivo_entrada += el
+
+        """
+        # Consumiendo la API
+        url = 'http://127.0.0.1:8800/cargar'
+        archivo_xml = {'texto': "".join(archivo_entrada)}
+        x = requests.post(url, data = archivo_xml)
+        print(x.text)
+        """
+
+        archivos_cargados.append(archivo_entrada)
+        # Retornando Templeate
+        main = loader.get_template('index.html')
+        documento = main.render({'respuesta': "".join(archivo_entrada)})
+        return HttpResponse(documento)
+
+@csrf_exempt
+def archivo2(request):
+    # Consumiendo la API
+    url = 'http://127.0.0.1:8800/cargar'
+    archivo_xml = {'texto': "".join(archivos_cargados[0])}
+    x = requests.post(url, data = archivo_xml)
+    print(x.text)
+    main = loader.get_template('index.html')
+    documento = main.render({'respuesta': "".join(archivos_cargados[0])})
+    return HttpResponse(documento)
+
+@csrf_exempt
+def consultar(request):
+    response = requests.get('http://127.0.0.1:8800/generar')
+    main = loader.get_template('index.html')
+    archivos_cargados.append(response.text)
+    documento = main.render({"respuesta": archivos_cargados[0], "respuesta_estadistica": response.text})
+    return HttpResponse(documento)
+
+@csrf_exempt
+def docu(request):
+    try:
+        return FileResponse(open('C:/Users/Humberto Morales/OneDrive/Escritorio/Rodrigo/Documentacion/EnsayoIPC2.pdf', 'rb'), content_type='application/pdf')
+    except FileNotFoundError:
+        raise Http404()
--- server/app.py
+++ server/app.py
@@ -1,34 +1,39 @@
 from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash, jsonify, send_from_directory
 from werkzeug.utils import secure_filename
-from evento import Evento
+from evento import Error, Evento
+import xmltodict
+import re
 import os
+import matplotlib.pyplot as plt
+
+eventos = []
+filtro_fecha = {}
+cont_repor = {}
+cont_err = {}
+fechas = []
+
+reportado_re = r'Reportado\spor:\s"[a-zA-Z|\s]+[0-9]"\s[a-zA-Z]+@([a-zA-Z]|.)+[a-zA-Z]+'
+afectados_re = r'Usuarios\safectados:\s([a-zA-Z]+@([a-zA-Z]|.)+[a-zA-Z]+,?)+'
+evento_re = r'<EVENTO>'
+evento1_re = r'</EVENTO>'
+eventos_re = r'<EVENTOS>'
+fecha_re = r'[0-9][0-9]/[0-9][0-9]/[0-9][0-9]'
+error_re = r'Error:\s[0-9]*\s-\s([a-zA-Z]|\s)+'
+correo_re = r'[a-zA-Z0-9]+@([a-zA-Z]|.)+[a-zA-Z]+,?'
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = os.path.abspath("./uploads")
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-
-"""
-@app.route('/archivo', methods=['POST'])
-def select():
-    nuevo_viaje={
-        "nombre": request.json['nombre'],
-        "hora": request.json['hora'],
-        "precio": request.json['precio']
-    }
-    vuelos.append(nuevo_viaje)
-    return jsonify({"peticion": vuelos})
-"""
-
 @app.route('/inicio')
 def saludar():
-    return jsonify({'message': '10'})
+    return '10'
 
+"""
 @app.route('/cargarArchivo', methods=['GET', 'POST'])
 def seleccionar():
     contenido = ''
-    eventos = []
     if request.method == "POST":
         f = request.files['ourfile']
         filename = f.filename
@@ -38,15 +43,225 @@ def seleccionar():
         for lin in archivo:
             contenido += lin
         return contenido
+"""
 
-@app.route('/prueba', methods=['GET', 'POST'])
-def nuevo():
-    if request.method == "POST":
-        nuevo_viaje={
-            "nombre": request.json['nombre'],
-            "hora": request.json['hora'],
-            "precio": request.json['precio']
-        }
+@app.route("/cargar", methods=['GET', 'POST'])
+def parse_xml():
+    retorno = ''
+    xml_data = request.form['texto']
+    cadena = str(xml_data)
+    f = cadena.split('\n')
+
+    aux_entrada = []
+    for el in f:
+        aux_entrada.append(el.strip())
+        
+    nuevo = False
+    for el in aux_entrada:
+        if re.search(eventos_re, el):
+            continue
+        elif re.search(evento_re, el):
+            nuevo = True
+        elif re.search(fecha_re, el):
+            aux_fecha = el.replace('\t', '')
+        elif re.search('Reportado\spor:\s', el):
+            #aux_repor = el.split(', ')
+            aux_repor_1 = ''
+            aux_repor = el
+            aux_repor = aux_repor.replace('Reportado por: ', '')
+            aux_repor = aux_repor.replace('<', '')
+            aux_repor = aux_repor.replace('>', '')
+            aux_repor = aux_repor.replace('"', '')
+            aux_repor = aux_repor.replace('"', '')
+            aux_repor = aux_repor.replace('\t', '')
+            if re.search(correo_re, aux_repor):
+                aux_repor_1 = re.search(correo_re, aux_repor).group()
+        elif re.search('Usuarios\safectados:\s', el):
+            aux_afec_re = []
+            aux_afec = el
+            aux_afec = aux_afec.replace('Usuarios afectados: ', '')
+            aux_afec = aux_afec.replace('\t', '')
+            aux_afec = aux_afec.split(', ')
+            for mint in aux_afec:
+                mint = mint.replace('<', '')
+                mint = mint.replace('>', '')
+                if re.search(correo_re, mint):
+                    aux_afec_re.append(re.search(correo_re, mint).group())
+        elif re.search('Error: ', el):
+            aux_err = el
+            aux_err = aux_err.replace('Error: ', '')
+            aux_err = aux_err.replace('\t', '')
+            aux_err = aux_err.split(' - ')
+        elif re.search(evento1_re, el):
+            if nuevo == True:            
+                eventos.append(Evento(aux_fecha, aux_repor_1, aux_afec_re, Error(aux_err[0], aux_err[1])))
+                nuevo = False
+
+    for ev in eventos:
+        retorno += ev.imprimir()
+
+    #print(eventos)
+    return 'Eventos almacenados: ' + retorno
+    #print(xml_data)
+    #content_dict = xmltodict.parse(xml_data)
+    #return jsonify(content_dict)
+
+@app.route("/generar")
+def estadistics():
+    cad = ''
+    # Filtro por Fechas
+    cont = -1
+    aux_fecha = eventos[0].fecha
+    
+    for ev in eventos:
+        cont += 1
+        if cont == 0:
+            fechas.append(ev.fecha)
+            filtro_fecha[ev.fecha] = [ev]
+        else:
+            if ev.fecha in filtro_fecha:
+                filtro_fecha[ev.fecha].append(ev)
+            else:
+                filtro_fecha[ev.fecha] = [ev]
+                fechas.append(ev.fecha)
+
+    for fe in fechas:
+        cont1 = -1
+        eventos1 = filtro_fecha[fe]
+        cont_repor[fe] = []
+        aox = cont_repor[fe]
+        for even1 in eventos1:
+            cont1 += 1
+            if cont1 == 0:
+                aox.append({even1.empleado: 1})
+            else:
+                if str(even1.empleado) in str(aox):
+                    inde = -1
+                    for lol in aox:
+                        inde += 1
+                        if even1.empleado in lol:
+                            aux = aox[inde]
+                            pol = aux[even1.empleado]
+                            pol += 1
+                            aux[even1.empleado] = pol
+                else:
+                    aox.append({even1.empleado: 1})
+    #print(cont_repor)
+    for fe1 in fechas:
+        cont2 = -1
+        eventos2 = filtro_fecha[fe1]
+        cont_err[fe1] = []
+        aox1 = cont_err[fe1]
+        for even2 in eventos2:
+            cont2 += 1
+            if cont2 == 0:
+                aox1.append({even2.error.num: 1})
+            else:
+                if str(even2.error.num) in str(aox1):
+                    inde1 = -1
+                    for lol1 in aox1:
+                        inde1 += 1
+                        if even2.error.num in lol1:
+                            aux1 = aox1[inde1]
+                            pol1 = aux1[even2.error.num]
+                            pol1 += 1
+                            aux1[even2.error.num] = pol1
+                else:
+                    aox1.append({even2.error.num: 1})
+
+    with open(os.getcwd()+'/server/uploads/salida.xml', 'w') as re:
+        re.write('<ESTADISTICAS>' + '\n')
+        for fech in filtro_fecha:
+            re.write('  <ESTADISTICA>' + '\n')
+            re.write('      <FECHA>' + str(fech) + '<FECHA>' + '\n')
+            re.write('      <CANTIDAD_MENSAJES>' + str(len(filtro_fecha[fech])) + '<CANTIDAD_MENSAJES>' + '\n')
+            re.write('      <REPORTADO_POR>' + '\n')
+            for reportes in cont_repor[fech]:
+                for reporte in reportes:
+                    re.write('      <USUARIO>' + '\n')
+                    re.write('          <EMAIL>'+ str(reporte) +'<EMAIL>' + '\n')
+                    re.write('          <CANTIDAD_MENSAJES>'+ str(reportes[reporte]) +'</CANTIDAD_MENSAJES>' + '\n')
+                    re.write('      </USUARIO>' + '\n')
+            re.write('      </REPORTADO_POR>' + '\n')
+            re.write('      <AFECTADOS>' + '\n')
+            for eventosl in filtro_fecha[fech]:
+                for afectado in eventosl.afectados:
+                    re.write('          <AFECTADO>'+ str(afectado) +'</AFECTADO>' + '\n')
+            re.write('      </AFECTADOS>' + '\n')
+            re.write('      <ERRORES>' + '\n')
+            for ers in cont_err[fech]:
+                for er in ers:
+                    re.write('          <ERROR>' + '\n')
+                    re.write('              <CODIGO>' + str(er) + '<CODIGO>' + '\n')
+                    re.write('              <CANTIDAD_MENSAJES>' + str(ers[er]) + '<CANTIDAD_MENSAJES>' + '\n')
+                    re.write('          <ERROR>' + '\n')
+            re.write('      </ERRORES>' + '\n')
+            re.write('  </ESTADISTICA>' + '\n')
+        re.write('</ESTADISTICAS>' + '\n')
+        re.close()
+
+    repuesta_nueva = open(os.getcwd()+'/server/uploads/salida.xml', 'r')
+    for valores in repuesta_nueva.readlines():
+        cad += valores
+    return cad
+
+"""
+@app.route("/graf")
+def grafico():
+    plt.plot([1, 2, 3, 4])
+    plt.ylabel('some numbers')
+    plt.show()
+    return 'Funciona'
+"""
+
+@app.route("/grafico_errores", methods=['GET', 'POST'])
+def graf():
+    cod = request.form['codigo']
+    cod = str(cod)
+    fechas = []
+    valores = []
+    titulo = 'Error: ' + str(cod)
+    #cont_err = {'15/01/20':[{'2001': 2}, {'1001': 1}], '16/01/20':[{'2001': 1}, {'1001': 1}]}
+    for fecha in cont_err:
+        for errores in cont_err[fecha]:
+            inde1x = -1
+            for err in errores:
+                inde1x += 1
+                if cod in err:
+                    valores.append(errores[cod])
+                    fechas.append(fecha)
+
+    plt.title(titulo)
+    plt.bar(fechas, valores)
+    plt.show()
+    return 'Grafica Realizada'
+
+@app.route("/graficar_fechas", methods=['GET', 'POST'])
+def frag():
+    date = request.form['fecha']
+    date = str(date)
+    ususarios = []
+    valores1 = []
+    titulo1 = 'Fecha: ' + str(date)
+    for user in cont_repor[date]:
+        for val in user:
+            ususarios.append(val)
+            valores1.append(user[val])
+
+    plt.title(titulo1)
+    plt.bar(ususarios, valores1)
+    plt.show()
+    return 'Grafica Generada'
+
+@app.route("/reset")
+def borrar():
+    eventos.clear()
+    filtro_fecha.clear()
+    cont_repor.clear()
+    cont_err.clear()
+    fechas.clear()
+    return jsonify({'message': 'Servidor reseteado'})
+
 
 if __name__ == '__main__':
     app.run(debug = True, port= 8800)
